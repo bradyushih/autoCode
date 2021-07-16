186e64557103fdba8d9f259c243da8825bd8c8f9
bradyushih <58912172+bradyushih@users.noreply.github.com>
2021-07-16 15:12:57 +0800

    Update fullWidthTohalWidth.js

--- fullWidthTohalWidth.js
+++ fullWidthTohalWidth.js
@@ -1,14 +1,46 @@
-function numIsFullWidth(val){
-		let gap = '０'.charCodeAt(0) - '0'.charCodeAt(0);
-		let min = '０'.charCodeAt(0);
-		let max = '９'.charCodeAt(0);
-		let halWidth = '';
-		[...val].forEach( c => {
-			let charCode = c.charCodeAt(0);
-			if(min<=charCode && charCode<=max){
-				charCode -= gap;
-				halWidth += String.fromCharCode(charCode)
-			}
+//0~9    ０~９         A~Za~z  Ａ~Ｚａ~ｚ
+	//48~57  65296~65305  65~122  65313~65370
+	let charCodeParams = ccp ={
+		newHalWidth : '',
+		numGap      : '０'.charCodeAt(0) - '0'.charCodeAt(0),
+		alphGap     : 'Ａ'.charCodeAt(0) - 'A'.charCodeAt(0),
+		numMin 		: '０'.charCodeAt(0),
+	    numMax 		: '９'.charCodeAt(0),
+		alphMin 	: 'Ａ'.charCodeAt(0),
+		alphMax 	: 'ｚ'.charCodeAt(0),
+	}
+	function FullWidth2HalWidth(str){
+		[...str].forEach( word => {
+			let charCode = word.charCodeAt(0);
+				switch (isFullWidth(charCode).type){
+					case('num'):
+						charCode -= ccp.numGap;
+						ccp.newHalWidth += String.fromCharCode(charCode)
+						break;
+					case('alph'):
+						charCode -= ccp.alphGap;
+						ccp.newHalWidth += String.fromCharCode(charCode)
+						break;
+					default:
+						ccp.newHalWidth += word;
+						break;
+				}
 		});
-		return halWidth;
+		return ccp.newHalWidth;
+	}
+	
+	function isFullWidth(charCode){
+		let res ={
+// 					'status': null, 
+					'type': null
+				};
+		if((ccp.numMin<=charCode && charCode<=ccp.numMax)){
+// 			res.status = true;
+			res.type = 'num';
+		}
+		if((ccp.alphMin<=charCode && charCode<=ccp.alphMax)){
+// 			res.status = true;
+			res.type = 'alph';
+		}
+		return res;
 	}
